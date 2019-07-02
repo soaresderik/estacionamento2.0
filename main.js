484c001721a5e4ae564cf0a3eafb39bb504f3ffc
Andre soares <codigoaberto.info@gmail.com>
2019-07-02 11:39:07 -0300

    fix: rename some variables

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
 (function(){
     const $ = q => document.querySelector(q);
 
-    function convertPeriod(millis) {
-        var min = Math.floor(millis / 60000);
-        var sec = Math.floor((millis % 60000) / 1000);
+    function convertPeriod(mil) {
+        var min = Math.floor(mil / 60000);
+        var sec = Math.floor((mil % 60000) / 1000);
         return `${min}m e ${sec}s`;
     };
 
     function renderGarage () {
-        const patio = getPatio();
+        const garage = getGarage();
         $("#garage").innerHTML = "";
-        patio.forEach(c => addCarToGarage(c))
+        garage.forEach(c => addCarToGarage(c))
     };
 
     function addCarToGarage (car) {
         const row = document.createElement("tr");
         row.innerHTML = `
-            <td>${car.veiculo}</td>
-            <td>${car.placa}</td>
+            <td>${car.name}</td>
+            <td>${car.licence}</td>
             <td data-time="${car.time}">
                 ${new Date(car.time)
                         .toLocaleString('pt-BR', { 
@@ -36,37 +36,39 @@
         let period = new Date() - new Date(info[2].dataset.time);
         period = convertPeriod(period);
 
-        const placa = info[1].textContent;
-        const msg = `O veículo ${info[0].textContent} de placa ${placa} permaneceu ${period} estacionado. \n\n Deseja encerrar?`;
+        const licence = info[1].textContent;
+        const msg = `O veículo ${info[0].textContent} de placa ${licence} permaneceu ${period} estacionado. \n\n Deseja encerrar?`;
 
         if(!confirm(msg)) return;
         
-        const garage = getPatio().filter(c => c.placa !== placa);
-        localStorage.patio = JSON.stringify(garage);
+        const garage = getGarage().filter(c => c.licence !== licence);
+        localStorage.garage = JSON.stringify(garage);
         
         renderGarage();
     };
 
-    const getPatio = () => localStorage.patio ? JSON.parse(localStorage.patio) : [];
+    const getGarage = () => localStorage.garage ? JSON.parse(localStorage.garage) : [];
 
     renderGarage();
     $("#send").addEventListener("click", e => {
-        const veiculo = $("#veiculo").value;
-        const placa = $("#placa").value;
+        const name = $("#name").value;
+        const licence = $("#licence").value;
 
-        if(!veiculo || !placa){
+        if(!name || !licence){
             alert("Os campos são obrigatórios.");
             return;
         }   
 
-        const data = { veiculo, placa, time: new Date() };
+        const card = { name, licence, time: new Date() };
 
-        const patio = getPatio();
-        patio.push(data);
+        const garage = getGarage();
+        garage.push(card);
 
-        localStorage.patio = JSON.stringify(patio);
+        localStorage.garage = JSON.stringify(garage);
 
-        addCarToGarage(data);
+        addCarToGarage(card);
+        $("#name").value = "";
+        $("#licence").value = "";
     });
 
     $("#garage").addEventListener("click", (e) => {
