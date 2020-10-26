b1bf7b1235745ec9a61cb9e745af4b99d4b2c93e
andre <codigoaberto.info@gmail.com>
2020-10-26 12:05:13 -0300

    feat criando versão orientada a objetos

--- main.js
+++ main.js
@@ -1,78 +1,112 @@
-(function(){
-    const $ = q => document.querySelector(q);
+class EstacionamentoFront {
+  constructor($) {
+    this.$ = $;
+    this.estacionamento = new Estacionamento();
+  }
 
-    function convertPeriod(mil) {
-        var min = Math.floor(mil / 60000);
-        var sec = Math.floor((mil % 60000) / 1000);
-        return `${min}m e ${sec}s`;
+  adicionar(carro, salvar = false) {
+    this.estacionamento.adicionar(carro);
+
+    const row = document.createElement("tr");
+    row.innerHTML = `
+                <td>${carro.nome}</td>
+                <td>${carro.placa}</td>
+                <td data-time="${carro.entrada}">
+                    ${new Date(carro.entrada).toLocaleString("pt-BR", {
+                      hour: "numeric",
+                      minute: "numeric",
+                    })}
+                </td>
+                <td>
+                    <button class="delete">x</button>
+                </td>
+            `;
+
+    if (salvar) {
+      this.estacionamento.salvar();
+    }
+
+    this.$("#garage").appendChild(row);
+  }
+
+  encerrar(cells) {
+    const veiculo = {
+      nome: cells[0].textContent,
+      placa: cells[1].textContent,
+      tempo: new Date() - new Date(cells[2].dataset.time),
     };
 
-    function renderGarage () {
-        const garage = getGarage();
-        $("#garage").innerHTML = "";
-        garage.forEach(c => addCarToGarage(c))
-    };
+    this.estacionamento.encerrar(veiculo);
+  }
 
-    function addCarToGarage (car) {
-        const row = document.createElement("tr");
-        row.innerHTML = `
-            <td>${car.name}</td>
-            <td>${car.licence}</td>
-            <td data-time="${car.time}">
-                ${new Date(car.time)
-                        .toLocaleString('pt-BR', { 
-                            hour: 'numeric', minute: 'numeric' 
-                })}
-            </td>
-            <td>
-                <button class="delete">x</button>
-            </td>
-        `;
+  render() {
+    this.$("#garage").innerHTML = "";
+    this.estacionamento.patio.forEach((c) => this.adicionar(c));
+  }
+}
 
-        $("#garage").appendChild(row);
-    };
+class Estacionamento {
+  constructor() {
+    this.patio = localStorage.patio ? JSON.parse(localStorage.patio) : [];
+  }
 
-    function checkOut(info) {
-        let period = new Date() - new Date(info[2].dataset.time);
-        period = convertPeriod(period);
+  adicionar(carro) {
+    this.patio.push(carro);
+  }
 
-        const licence = info[1].textContent;
-        const msg = `O veículo ${info[0].textContent} de placa ${licence} permaneceu ${period} estacionado. \n\n Deseja encerrar?`;
+  encerrar(info) {
+    const tempo = this.calcTempo(info.tempo);
 
-        if(!confirm(msg)) return;
-        
-        const garage = getGarage().filter(c => c.licence !== licence);
-        localStorage.garage = JSON.stringify(garage);
-        
-        renderGarage();
-    };
+    const msg = `
+      O veículo ${info.nome} de placa ${info.placa} permaneceu ${tempo} estacionado.
+      \n\n Deseja encerrar?
+    `;
 
-    const getGarage = () => localStorage.garage ? JSON.parse(localStorage.garage) : [];
+    if (!confirm(msg)) return;
 
-    renderGarage();
-    $("#send").addEventListener("click", e => {
-        const name = $("#name").value;
-        const licence = $("#licence").value;
+    this.patio = this.patio.filter((carro) => carro.placa !== info.placa);
 
-        if(!name || !licence){
-            alert("Os campos são obrigatórios.");
-            return;
-        }   
+    this.salvar();
+  }
 
-        const card = { name, licence, time: new Date() };
+  calcTempo(mil) {
+    var min = Math.floor(mil / 60000);
+    var sec = Math.floor((mil % 60000) / 1000);
+    return `${min}m e ${sec}s`;
+  }
 
-        const garage = getGarage();
-        garage.push(card);
+  salvar() {
+    console.log("Salvando...");
+    localStorage.patio = JSON.stringify(this.patio);
+  }
+}
 
-        localStorage.garage = JSON.stringify(garage);
+(function () {
+  const $ = (q) => document.querySelector(q);
+  const estacionamento = new EstacionamentoFront($);
+  estacionamento.render();
 
-        addCarToGarage(card);
-        $("#name").value = "";
-        $("#licence").value = "";
-    });
+  $("#send").addEventListener("click", (e) => {
+    const nome = $("#name").value;
+    const placa = $("#licence").value;
 
-    $("#garage").addEventListener("click", (e) => {
-        if(e.target.className === "delete")
-            checkOut(e.target.parentElement.parentElement.cells);
-    });
-})()
+    if (!nome || !placa) {
+      alert("Os campos são obrigatórios.");
+      return;
+    }
+
+    const carro = { nome, placa, entrada: new Date() };
+
+    estacionamento.adicionar(carro, true);
+
+    $("#name").value = "";
+    $("#licence").value = "";
+  });
+
+  $("#garage").addEventListener("click", (e) => {
+    if (e.target.className === "delete") {
+      estacionamento.encerrar(e.target.parentElement.parentElement.cells);
+      estacionamento.render();
+    }
+  });
+})();
